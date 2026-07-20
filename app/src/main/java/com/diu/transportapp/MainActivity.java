5b95205ffb7d8b4a1f66f4b42a32fcc4e97b7f86
Ahsan Habib <habibahsan9503@gmail.com>
2026-07-21 01:07:19 +0600

    Updated UI and added new features

--- app/src/main/java/com/diu/transportapp/MainActivity.java
+++ app/src/main/java/com/diu/transportapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.diu.transportapp;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -12,13 +16,25 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Night Mode
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Loading Text Animation
+        TextView loadingText = findViewById(R.id.loadingText);
+        ObjectAnimator animator = ObjectAnimator.ofFloat(loadingText, View.ALPHA, 1.0f, 0.2f);
+        animator.setDuration(800); // 0.8 second
+        animator.setRepeatMode(ObjectAnimator.REVERSE);
+        animator.setRepeatCount(ObjectAnimator.INFINITE);
+        animator.start();
     }
 }
